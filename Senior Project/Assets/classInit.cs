4ae529506b29a2ce5fdf504ec3fcf3f38187b157
Addison Ivan <addison@merillian.com>
2014-10-08 21:58:15 -0700

    Fixed sync with unnecessary files
    Also added classinit.cs with basic Class function.

---
+++ Senior Project/Assets/classInit.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class classInit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+What I understand so far, still learning/trying to understand C#.
+
+
+namespace mainCube
+{
+	public struct cubeFace
+	{
+		public int A;
+		public int B;
+		public int C;
+		public int D;
+	}
+	public class cubeBuilder
+	{
+		public string Cubename { get; set; }
+		public Vector[] cubeVertices {get; private set;}
+		public Face[] cubeFaces {get; private set;}
+		public Vector[] Position {get; set; }
+	}
+}
+// Only called simple class... expand next meeting.
+//Draw cube TBD
